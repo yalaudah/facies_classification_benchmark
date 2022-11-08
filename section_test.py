c406d40f89b6808d44c6143efffae1a550c83c14
Rafael Vareto <rafaelvareto@gmail.com>
2022-11-08 14:56:37 +0100

    Adapting code to preliminary execution on  server - part I

--- section_test.py
+++ section_test.py
@@ -1,12 +1,13 @@
 import argparse
-from os.path import join as pjoin
 
-import numpy as np
+
+import numpy
 import torch
-import torch.nn.functional as F
+import torchvision
+
+from os.path import join as pjoin
 from tensorboardX import SummaryWriter
 
-import torchvision.utils as vutils
 from core.loader.data_loader import *
 from core.metrics import runningScore
 from core.utils import np_to_tb
@@ -21,8 +22,7 @@ def test(args):
     model = model.to(device)  # Send to GPU if available
     writer = SummaryWriter(log_dir=log_dir)
 
-    class_names = ['upper_ns', 'middle_ns', 'lower_ns',
-                   'rijnland_chalk', 'scruff', 'zechstein']
+    class_names = ['upper_ns', 'middle_ns', 'lower_ns', 'rijnland_chalk', 'scruff', 'zechstein']
     running_metrics_overall = runningScore(6)
 
     if "both" in args.split: 
@@ -32,7 +32,7 @@ def test(args):
 
     for sdx, split in enumerate(splits):
         # define indices of the array
-        labels = np.load(pjoin('data', 'test_once', split + '_labels.npy'))
+        labels = numpy.load(pjoin('data', 'test_once', split + '_labels.npy'))
         irange, xrange, depth = labels.shape
 
         if args.inline:
@@ -54,9 +54,7 @@ def test(args):
         file_object.write('\n'.join(list_test))
         file_object.close()
 
-        test_set = section_loader(is_transform=True,
-                                  split=split,
-                                  augmentations=None)
+        test_set = section_loader(is_transform=True, split=split, augmentations=None)
         n_classes = test_set.n_classes
 
         test_loader = data.DataLoader(test_set,
@@ -86,25 +84,21 @@ def test(args):
                 numbers = [0, 99, 149, 399, 499]
 
                 if i in numbers:
-                    tb_original_image = vutils.make_grid(
-                        image_original[0][0], normalize=True, scale_each=True)
-                    writer.add_image('test/original_image',
-                                     tb_original_image, i)
+                    tb_original_image = torchvision.utils.make_grid(image_original[0][0], normalize=True, scale_each=True)
+                    writer.add_image('test/original_image', tb_original_image, i)
 
                     labels_original = labels_original.numpy()[0]
-                    correct_label_decoded = test_set.decode_segmap(np.squeeze(labels_original))
-                    writer.add_image('test/original_label',
-                                     np_to_tb(correct_label_decoded), i)
-                    out = F.softmax(outputs, dim=1)
+                    correct_label_decoded = test_set.decode_segmap(numpy.squeeze(labels_original))
+                    writer.add_image('test/original_label', np_to_tb(correct_label_decoded), i)
+                    out = torch.nn.functional.softmax(outputs, dim=1)
 
                     # this returns the max. channel number:
                     prediction = out.max(1)[1].cpu().numpy()[0]
                     # this returns the confidence:
                     confidence = out.max(1)[0].cpu().detach()[0]
-                    tb_confidence = vutils.make_grid(
-                        confidence, normalize=True, scale_each=True)
+                    tb_confidence = torchvision.utils.make_grid(confidence, normalize=True, scale_each=True)
 
-                    decoded = test_set.decode_segmap(np.squeeze(prediction))
+                    decoded = test_set.decode_segmap(numpy.squeeze(prediction))
                     writer.add_image('test/predicted', np_to_tb(decoded), i)
                     writer.add_image('test/confidence', tb_confidence, i)
 
@@ -117,25 +111,21 @@ def test(args):
 
                     for channel in range(0, len(class_names)):
                         decoded_channel = unary[0][channel]
-                        tb_channel = vutils.make_grid(decoded_channel, normalize=True, scale_each=True)
+                        tb_channel = torchvision.utils.make_grid(decoded_channel, normalize=True, scale_each=True)
                         writer.add_image(f'test_classes/_{class_names[channel]}', tb_channel, i)
 
         # get scores and save in writer()
         score, class_iou = running_metrics_split.get_scores()
 
         # Add split results to TB:
-        writer.add_text(f'test__{split}/',
-                        f'Pixel Acc: {score["Pixel Acc: "]:.3f}', 0)
+        writer.add_text(f'test__{split}/', f'Pixel Acc: {score["Pixel Acc: "]:.3f}', 0)
         for cdx, class_name in enumerate(class_names):
             writer.add_text(
                 f'test__{split}/', f'  {class_name}_accuracy {score["Class Accuracy: "][cdx]:.3f}', 0)
 
-        writer.add_text(
-            f'test__{split}/', f'Mean Class Acc: {score["Mean Class Acc: "]:.3f}', 0)
-        writer.add_text(
-            f'test__{split}/', f'Freq Weighted IoU: {score["Freq Weighted IoU: "]:.3f}', 0)
-        writer.add_text(f'test__{split}/',
-                        f'Mean IoU: {score["Mean IoU: "]:0.3f}', 0)
+        writer.add_text(f'test__{split}/', f'Mean Class Acc: {score["Mean Class Acc: "]:.3f}', 0)
+        writer.add_text(f'test__{split}/', f'Freq Weighted IoU: {score["Freq Weighted IoU: "]:.3f}', 0)
+        writer.add_text(f'test__{split}/', f'Mean IoU: {score["Mean IoU: "]:0.3f}', 0)
 
         running_metrics_split.reset()
 
@@ -145,27 +135,23 @@ def test(args):
     # Add split results to TB:
     writer.add_text('test_final', f'Pixel Acc: {score["Pixel Acc: "]:.3f}', 0)
     for cdx, class_name in enumerate(class_names):
-        writer.add_text(
-            'test_final', f'  {class_name}_accuracy {score["Class Accuracy: "][cdx]:.3f}', 0)
+        writer.add_text('test_final', f'  {class_name}_accuracy {score["Class Accuracy: "][cdx]:.3f}', 0)
 
-    writer.add_text(
-        'test_final', f'Mean Class Acc: {score["Mean Class Acc: "]:.3f}', 0)
-    writer.add_text(
-        'test_final', f'Freq Weighted IoU: {score["Freq Weighted IoU: "]:.3f}', 0)
+    writer.add_text('test_final', f'Mean Class Acc: {score["Mean Class Acc: "]:.3f}', 0)
+    writer.add_text('test_final', f'Freq Weighted IoU: {score["Freq Weighted IoU: "]:.3f}', 0)
     writer.add_text('test_final', f'Mean IoU: {score["Mean IoU: "]:0.3f}', 0)
 
     print('--------------- FINAL RESULTS -----------------')
     print(f'Pixel Acc: {score["Pixel Acc: "]:.3f}')
     for cdx, class_name in enumerate(class_names):
-        print(
-            f'     {class_name}_accuracy {score["Class Accuracy: "][cdx]:.3f}')
+        print(f'     {class_name}_accuracy {score["Class Accuracy: "][cdx]:.3f}')
     print(f'Mean Class Acc: {score["Mean Class Acc: "]:.3f}')
     print(f'Freq Weighted IoU: {score["Freq Weighted IoU: "]:.3f}')
     print(f'Mean IoU: {score["Mean IoU: "]:0.3f}')
 
     # Save confusion matrix: 
     confusion = score['confusion_matrix']
-    np.savetxt(pjoin(log_dir,'confusion.csv'), confusion, delimiter=" ")
+    numpy.savetxt(pjoin(log_dir,'confusion.csv'), confusion, delimiter=" ")
 
     writer.close()
     return
